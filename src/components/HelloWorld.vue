f0aaaae6de1602110a35243e85eee70f7949ae22
Chriz NM <chriznm@gmail.com>
2024-05-02 21:53:25 -0600

    Working version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,17 @@
-<script setup lang="ts">
-let pzs: number = 1
-let precioSetWon: number = 40000
-let envio: number = 1800
-let fee: number = 2000
-let krw: number = 1
-let usd: number = 0.000869565217391304
-let mxn: number = 17.1325
-let wonTotal: number = fee + precioSetWon + envio
-let setEnUsd: number = Math.round(wonTotal * usd * 100) / 100
-let setEnMxn: number = Math.round(setEnUsd * mxn)
-let precioPCMxn: number = setEnMxn / pzs
-let precioFinalPorSet: number = Math.round(precioPCMxn * 1.334)
-let precioFinalPorPc: number = precioFinalPorSet / pzs
-let ganancia: number = precioFinalPorPc - setEnMxn
-</script>
-
 <template>
   <div class="classContainer">
     <div>
-      <v-text-field v-model="pzs" label="PZS" width="500px" />
+      <v-text-field v-model="quantity" label="PZS" width="500px" />
       <v-text-field v-model="precioSetWon" label="Precio Set Won" width="500px" />
       <v-text-field v-model="envio" label="Envio" width="500px" />
       <v-text-field v-model="fee" label="FEE" width="500px" />
-      <v-text-field v-model="wonTotal" label="WON TOTAL" width="500px" :readonly="true" />
+      <v-text-field
+        :value="wonTotal"
+        label="WON TOTAL"
+        width="500px"
+        :active="true"
+        :readonly="true"
+      />
       <v-text-field v-model="setEnUsd" label="SET EN USD" width="500px" />
       <v-text-field v-model="setEnMxn" label="SET EN MXN" width="500px" />
       <v-text-field v-model="precioPCMxn" label="Precio PC mxn" width="500px" />
@@ -38,6 +27,43 @@ let ganancia: number = precioFinalPorPc - setEnMxn
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref, computed, type Ref, type ComputedRef } from 'vue'
+
+const quantity: Ref<number> = ref(1)
+const envio: Ref<number | undefined> = ref()
+const fee: Ref<number | undefined> = ref()
+const krw: Ref<number> = ref(0)
+const usd: Ref<number> = ref(0)
+const mxn: Ref<number> = ref(0)
+const precioSetWon: Ref<number | undefined> = ref()
+
+//let pzs: number = 1
+//let precioSetWon: number = 40000
+//let envio: number = 1800
+//let fee: number = 2000
+//let krw: number = 1
+//let usd: number = 0.000869565217391304
+//let mxn: number = 17.1325
+
+const wonTotal: ComputedRef<number> = computed<number>(
+  () => Number(fee.value) + Number(precioSetWon.value) + Number(envio.value) || 0
+)
+const setEnUsd: ComputedRef<number> = computed<number>(
+  () => Math.round(wonTotal.value * usd.value * 100) / 100
+)
+const setEnMxn: ComputedRef<number> = computed<number>(() => Math.round(setEnUsd.value * mxn.value))
+const precioPCMxn: ComputedRef<number> = computed<number>(() => setEnMxn.value / quantity.value)
+const precioFinalPorSet: ComputedRef<number> = computed<number>(() =>
+  Math.round(precioPCMxn.value * 1.334)
+)
+const precioFinalPorPc: ComputedRef<number> = computed<number>(
+  () => precioFinalPorSet.value / quantity.value
+)
+const ganancia: ComputedRef<number> = computed<number>(
+  () => precioFinalPorPc.value - setEnMxn.value
+)
+</script>
 <style scoped>
 .classContainer {
   display: flex;
